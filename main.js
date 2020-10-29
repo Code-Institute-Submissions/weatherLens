43e04d73a088c8ea04b9be04e55eabef9cd9884a
Ted-Williams <ted_Williams@hotmail.co.uk>
2020-10-29 19:04:07 +0000

    main.js beautified

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 const api = {
-    key: "03e844bff36a172614ecb788fd7c7fd8",
-    base: "https://api.openweathermap.org/data/2.5/"
+  key: "03e844bff36a172614ecb788fd7c7fd8",
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const enquiry = document.querySelector('.enquiry');
 enquiry.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
-    if (evt.keyCode == 13) {
-        getResults(enquiry.value);
-    }
+  if (evt.keyCode == 13) {
+    getResults(enquiry.value);
+  }
 }
 
 function getResults(query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
-        return weather.json();
+      return weather.json();
     }).then(displayResults);
 }
 
-function displayResults (weather) {
-    let area = document.querySelector('#place .area');
-    area.innerText = `${weather.name}, ${weather.sys.country}`;
+function displayResults(weather) {
+  let area = document.querySelector('#place .area');
+  area.innerText = `${weather.name}, ${weather.sys.country}`;
 
-    let now = new Date();
-    let date = document.querySelector("#place .day");
-    date.innerText = dateBuilder(now);
+  let now = new Date();
+  let date = document.querySelector("#place .day");
+  date.innerText = dateBuilder(now);
 
-    let temp = document.querySelector('#conditions-now .temperature');
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  let temp = document.querySelector('#conditions-now .temperature');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
-    let weatherElement = document.querySelector('#conditions-now .conditions');
-    weatherElement.innerText =  weather.weather[0].main;
+  let weatherElement = document.querySelector('#conditions-now .conditions');
+  weatherElement.innerText = weather.weather[0].main;
 
-    let temperatureDifference = document.querySelector('.temperature-difference');
-    temperatureDifference.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  let temperatureDifference = document.querySelector('.temperature-difference');
+  temperatureDifference.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
-function dateBuilder (d) {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function dateBuilder(d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`;
+  return `${day} ${date} ${month} ${year}`;
 }
