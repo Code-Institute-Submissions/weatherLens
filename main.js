8bfb741587ba45e1caa97254113d817d39cde973
Ted-Williams <ted_Williams@hotmail.co.uk>
2020-10-29 18:38:44 +0000

    enquiry and key press event query added to main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const api = {
     key: "03e844bff36a172614ecb788fd7c7fd8",
-    base: "http://api.openweathermap.org/data/2.5/"
+    base: "https://api.openweathermap.org/data/2.5/"
 }
+
+const enquiry = document.querySelector('.enquiry');
+enquiry.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(enquiry.value);
+    }
+}
+
