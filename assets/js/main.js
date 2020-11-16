54464211a909cf0e2ba07b9ebb35defa4c013e31
Ted-Williams <ted_Williams@hotmail.co.uk>
2020-11-16 15:36:02 +0000

    form changed to div in index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,28 @@
 const enquiryRef = document.querySelector('#enquiry');
-const areaRef = document.querySelector('#place #area');
-const dateRef = document.querySelector ('#place #day');
-const weatherRef = document.querySelector ('#conditions-now #conditions');
-const tempRef = document.querySelector ('#conditions-now #temperature');
+const areaRef = document.querySelector('#area');
+const dateRef = document.querySelector ('#day');
+const weatherRef = document.querySelector ('#conditions');
+const tempRef = document.querySelector ('#temperature');
 const tempDiffRef = document.querySelector ('#temperature-difference');
 
 const api = {
-  key: "03e844bff36a172614ecb788fd7c7fd8",
-  base: "https://api.openweathermap.org/data/2.5/",
+  key: '03e844bff36a172614ecb788fd7c7fd8',
+  base: 'https://api.openweathermap.org/data/2.5/',
 };
 
 
 enquiryRef.addEventListener('keypress', checkIfEnter);
 
 function checkIfEnter(event){
-  if (event.keyCode === 13) {
+  if(event.keyCode === 13) {
     getResults(enquiryRef.value);
   }
 }
 
 
-function getResults(city) {
+function getResults(city){
   fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
-if (!city.ok) {
-    throw Error (city.stausText);
-};
-    then((response) => response.json()) 
+    .then((response) => response.json()) 
     .then((weatherData) => {
     displayResults(weatherData);
     });
