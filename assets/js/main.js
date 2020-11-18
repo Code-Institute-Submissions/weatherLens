af4db17062b3b0a9d99f3ec5f6ee5625cede1334
Ted-Williams <ted_Williams@hotmail.co.uk>
2020-11-18 15:44:05 +0000

    reset view function and if weather.cod = 404 then display City not found. Please try again added

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ const weatherRef = document.querySelector ('#conditions');
 const tempRef = document.querySelector ('#temperature');
 const tempDiffRef = document.querySelector ('#temperature-difference');
 const searchRef = document.querySelector ('#searchBtn');
+const dateToday = new Date();
 
 // Api URL and key to access data 
 
@@ -32,25 +33,35 @@ function checkIfEnter(event){
 
 function getResults(city) {
     fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
-        .then(function (response) {
-            if (!response.ok) {
-                throw new Error ('error');
-            }
-        })
         .then((response) => response.json())
         .then((weatherData) => {
+            console.log(weatherData);
             displayResults(weatherData);
         })
-        .catch(function (error) {
-            alert ('Invalid location. Please try again. Maybe check the spelling');
+        .catch((error) => {
+            console.log(error)
+             resetView();
+            
         });
 }
 
+function resetView() {
+    areaRef.innerText = "";
+    dateRef.innerText = "";
+    tempRef.innerHTML = "";
+    weatherRef.innerText = "";
+    tempDiffRef.innerText = "";
+}
+
 function displayResults(weather) {
+    if (weather.cod === '404') {
+        resetView()
+        areaRef.innerHTML = "City not found. Please try again."
+    } else {
   areaRef.innerText = `${weather.name}, ${weather.sys.country}`;
-  const dateToday = new Date();
   dateRef.innerText = dateToday.toDateString();
   tempRef.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
   weatherRef.innerText = weather.weather[0].main;
   tempDiffRef.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
 }
