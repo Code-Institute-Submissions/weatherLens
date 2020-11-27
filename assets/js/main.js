f30fed9ff6e29849dcc75c559c8fe20771325fd8
Ted-Williams <ted_Williams@hotmail.co.uk>
2020-11-27 19:31:56 +0000

    comments deleted from main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-// list of variables 
-
 const enquiryRef = document.querySelector('#enquiry');
 const areaRef = document.querySelector('#area');
 const dateRef = document.querySelector ('#day');
@@ -9,27 +7,21 @@ const tempDiffRef = document.querySelector ('#temperature-difference');
 const searchRef = document.querySelector ('#searchBtn');
 const dateToday = new Date();
 
-// Api URL and key to access data 
-
 const api = {
   key: '03e844bff36a172614ecb788fd7c7fd8',
   base: 'https://api.openweathermap.org/data/2.5/',
 };
 
-// Event listeners for the enter button and a mouse click on the search button
-
-enquiryRef.addEventListener('keypress', checkIfEnter);    // Enter button when text is entered into the search box
-searchRef.addEventListener('click', checkIfEnter);       // Mouse clicking the search button when text is entered into the search box
 
 
+enquiryRef.addEventListener('keypress', checkIfEnter);    
+searchRef.addEventListener('click', checkIfEnter);      
 
 function checkIfEnter(event){
   if(event.keyCode === 13 || event.type === 'click') {
     getResults(enquiryRef.value);
   }
 }
-
-
 
 function getResults(city) {
     fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
