d1e22747767018c1fec989253224f6c8893d974e
Ted-Williams <ted_Williams@hotmail.co.uk>
2020-12-01 18:51:00 +0000

    returns added to getResults comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,7 @@ function checkIfEnter(event){
  * If the location that the user searches for is invalid, this function will catch the error
  * 
  * @param {string} city The location the user is searching to find out the weather conditions
+ * @returns {Object} The weather at the searched location
  */
 function getResults(city) {
     fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
