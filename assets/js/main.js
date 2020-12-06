6dc7ad239fab588a86e6ca3567ed03d5f2db20d5
Ted-Williams <ted_Williams@hotmail.co.uk>
2020-12-06 11:18:06 +0000

    All code beautified

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 const enquiryRef = document.querySelector('#enquiry');
 const areaRef = document.querySelector('#area');
-const dateRef = document.querySelector ('#day');
-const weatherRef = document.querySelector ('#conditions');
-const tempRef = document.querySelector ('#temperature');
-const tempDiffRef = document.querySelector ('#temperature-difference');
-const searchRef = document.querySelector ('#searchBtn');
+const dateRef = document.querySelector('#day');
+const weatherRef = document.querySelector('#conditions');
+const tempRef = document.querySelector('#temperature');
+const tempDiffRef = document.querySelector('#temperature-difference');
+const searchRef = document.querySelector('#searchBtn');
 const dateToday = new Date();
 
 const api = {
@@ -13,72 +13,77 @@ const api = {
 };
 
 
-enquiryRef.addEventListener('keypress', checkIfEnter);    
-searchRef.addEventListener('click', checkIfEnter);      
+enquiryRef.addEventListener('keypress', checkIfEnter);
+searchRef.addEventListener('click', checkIfEnter);
 
 
 /**
  * 
- * @param {object} event Triggered by the user either clicking the enter button or by using the mouse and clicking on the search icon
+ * @param {object} event Triggered by the user either clicking the enter button 
+ * or by using the mouse and clicking on the search icon
  */
 
-function checkIfEnter(event){
-  if(event.keyCode === 13 || event.type === 'click') {
-      if (!enquiryRef.value) return;
+function checkIfEnter(event) {
+  if (event.keyCode === 13 || event.type === 'click') {
+    if (!enquiryRef.value) return;
     getResults(enquiryRef.value);
   }
 }
 
 /**
  * This function will fetch the weather data from the open weather API
- * If the location that the user searches for is invalid, this function will catch the error
+ * If the location that the user searches for is invalid, 
+ * this function will catch the error
  * 
- * @param {string} city The location the user is searching to find out the weather conditions
- * @returns {Object} The weather at the searched location
+ * @param {string} city The location the user is searching 
+ * to find out the weather conditions
+ * 
+ * @fires displayResults 
  */
 
 function getResults(city) {
-    fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
-        .then((response) => response.json())
-        .then((weatherData) => {
-            displayResults(weatherData);
-        })
-        .catch((error) => {
-            console.log(error);
-             resetView();
-            
-        });
+  fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
+    .then((response) => response.json())
+    .then((weatherData) => {
+      displayResults(weatherData);
+    })
+    .catch((error) => {
+      console.log(error);
+      resetView();
+
+    });
 }
 
 /**
- * 
- * @param {any} clear  clears the weather data on the screen if there is an error
+ * @param {any} () clears the weather data on the screen if there is an error
  */
 
-function resetView(clear) {
-    areaRef.innerText = "";
-    dateRef.innerText = "";
-    tempRef.innerHTML = "";
-    weatherRef.innerText = "";
-    tempDiffRef.innerText = "";
+function resetView() {
+  areaRef.innerText = "";
+  dateRef.innerText = "";
+  tempRef.innerHTML = "";
+  weatherRef.innerText = "";
+  tempDiffRef.innerText = "";
 }
 
 
 /**
+ * displays area name, date, temperature, conditions and min and max temperature
  * 
- * @param {any} weather displays the weather conditions for the users searched location
- * @returns {Object} displays the location name, date, temperature, conditions and minimum and maximum temperature
+ * @param {any} weather displays the weather conditions 
+ * for the users searched location
  */
 
 function displayResults(weather) {
-    if (weather.cod === '404'|| weather.cod === '400') {
-        resetView();
-        areaRef.innerHTML = "City not found. Please try again.";
-    } else {
-  areaRef.innerText = `${weather.name}, ${weather.sys.country}`;
-  dateRef.innerText = dateToday.toDateString();
-  tempRef.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-  weatherRef.innerText = weather.weather[0].main;
-  tempDiffRef.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-}
+  if (weather.cod === '404' || weather.cod === '400') {
+    resetView();
+    areaRef.innerHTML = "City not found. Please try again.";
+  } else {
+    areaRef.innerText = `${weather.name}, ${weather.sys.country}`;
+    dateRef.innerText = dateToday.toDateString();
+    tempRef.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+    weatherRef.innerText = weather.weather[0].main;
+    tempDiffRef.innerText =
+      `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  }
 }
